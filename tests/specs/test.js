d71f48228cd475349426be1874ef0e3b648459b2
Ramakrishnan Kandasamy <rmkanda@thoughtworks.com>
2019-09-10 14:32:15 +0530

    Remove duplicate tests

--- tests/specs/test.js
+++ tests/specs/test.js
@@ -1,17 +1,7 @@
 const assert = require("assert");
 
-describe("Automation practice home page", function() {
+describe("Automation practice - Google Search", function() {
   it("should have the right title", async () => {
-    await browser.url("");
-    const title = await browser.getTitle();
-    assert.strictEqual(title, "Google");
-  });
-  it("should have the right titlea", async () => {
-    await browser.url("");
-    const title = await browser.getTitle();
-    assert.strictEqual(title, "Google");
-  });
-  it("should have the right titlee", async () => {
     await browser.url("");
     const title = await browser.getTitle();
     assert.strictEqual(title, "Google");
